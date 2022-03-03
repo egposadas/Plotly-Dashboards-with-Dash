e245248aee0c9f4a7a5122b9e0a77ce87f212b12
egposadas <egposadas@tesla.com>
2022-03-03 20:33:42 +0100

    add more items

--- 0-02-Pandas-Crash-Course/Pandas-Crash-Course.py
+++ 0-02-Pandas-Crash-Course/Pandas-Crash-Course.py
@@ -3,49 +3,36 @@
 ##################################
 # We'll use pandas more than numpy in the Course
 # so let's quickly go over a few main ideas with pandas!
-
 import pandas as pd
 import numpy as np
-
 
 # Reading in CSV files. Use the read_csv command.
 # More options: https://pandas.pydata.org/pandas-docs/stable/io.html
 df = pd.read_csv('salaries.csv')
-
 print(df)
-
 
 # You can select columns with a bracket call:
 print(df['Name'])
-
-
 print(df['Salary'])
-
 
 # Select multiple columns with a list of column names.
 # Since you are passing in a list, you see two sets of []
 print(df[['Name','Salary']])
 
-
 # Similar to NumPy, you can create calls of min(), max(), mean(), etc...
 # on a pandas dataframe.
-
 print(df['Age'].mean())
-
 
 # Just like Numpy, we can use conditional filtering to select rows that meet
 # certain critera. Like choosing rows where the Age value is greater than 30
-
 ser_of_bool = df['Age'] > 30
 print(ser_of_bool)
 
 # Use this filter of booleans to then select the rows
-
 age_filter = df['Age'] > 30
 
 # Pass it to the dataframe
 print(df[age_filter])
-
 
 # More commonly done all in one step:
 df[df['Age'] > 30]
@@ -61,5 +48,11 @@ df.info() # General info about your dataframe
 df.describe() # Statistics about your dataframe
 df.columns # Grab a list of all columns
 df.index # Create an index list
+
 # You can convert a numpy matrix to a dataframe with:
 mat = np.arange(50).reshape(5,10)
+df = pd.DataFrame(data=mat)
+
+mat = np.arange(10).reshape(5,2)
+df = pd.DataFrame(data=mat, columns=['A','B'])
+print(df)
