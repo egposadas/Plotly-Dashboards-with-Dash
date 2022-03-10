fbcbab93cc75e864dac84088ca734c0d8e37ce89
egposadas <egposadas@tesla.com>
2022-03-10 15:49:35 +0100

    add

--- 2-05-DashComponents/CoreComponents.py
+++ 2-05-DashComponents/CoreComponents.py
@@ -2,3 +2,60 @@
 # This provides examples of Dash Core Components.
 # Feel free to add things to it that you find useful.
 ######
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    children=[
+        dcc.Dropdown(
+            options=[
+                {'label': 'new York City', 'value': 'NYC'},
+                {'label': 'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value='MTL',
+        ),
+        
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': u'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value=['MTL', 'SF'],
+            multi=True
+        ),
+        
+        # SLIDER https://dash.plot.ly/dash-core-components/slider
+        html.Label('Slider'),
+        html.P(
+            dcc.Slider(
+                min=-10,
+                max=10,
+                step=0.5,
+                marks={i: i for i in range(-10,11)},
+                value=0
+            )
+        ),
+        # RADIO ITEMS https://dash.plot.ly/dash-core-components/radioitems
+        html.Label('Radio Items'),
+        dcc.RadioItems(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': 'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value='MTL'
+        )
+    ],
+    style = {'width':'50%'}
+    
+)
+
+if __name__ == '__main__':
+    app.run_server()
+    
